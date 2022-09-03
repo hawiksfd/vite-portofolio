0207eda3f4d1a168fffdc2661049bb8a96331084
hawik <hawiksyaef@gmail.com>
2022-09-03 17:19:47 +0700

    configur tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{jsx,js}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
