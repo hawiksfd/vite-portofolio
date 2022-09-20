43753e381111065725e032c19da032e02d66af2b
hawik <hawiksyaef@gmail.com>
2022-09-20 21:10:53 +0700

    konfigurasi service item

---
+++ src/data/services.jsx
@@ -0,0 +1,22 @@
+import { MdWeb } from 'react-icons/md';
+import { FaHandHoldingHeart } from 'react-icons/fa';
+import { VscCode } from 'react-icons/vsc';
+
+
+export default [
+    {
+        title: 'Web Design',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, tempora!',
+        icon: <MdWeb className='w-full h-full'></MdWeb>
+    },
+    {
+        title: 'UI Design',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, tempora!',
+        icon: <FaHandHoldingHeart className='w-full h-full'></FaHandHoldingHeart>
+    },
+    {
+        title: 'Web Development',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, tempora!',
+        icon: <VscCode className='w-full h-full'></VscCode>
+    }
+]
