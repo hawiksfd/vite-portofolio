85b5481cea32eff62faa5aa5017a3bd60117e08b
hawik <hawiksyaef@gmail.com>
2022-09-20 11:10:31 +0700

    konfigurasi Dark Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import HeroSection from './components/HeroSection';
 
 function App() {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      setTheme('dark');
+    }else{
+      setTheme('light');
+    }
+  }, []);
+
+  useEffect(() => {
+    if(theme === 'dark'){
+      document.documentElement.classList.add('dark');
+    }else{
+      document.documentElement.classList.remove('dark');
+
+    }
+  })
+
+  const handleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+  
   return (
+    <>
+    <button
+      type='button'
+      onClick={handleTheme}
+      className='fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md'
+    >{theme === 'dark' ? '🌙' : '☀️'}</button>
     <div className="font-inter bg:white dark:bg-slate-900">
       <HeroSection>
       </HeroSection>
     </div>
+    </>
   )
 }
 
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,9 +6,9 @@ const HeroSection = () => {
     flex-col py-20">
         <div className="text-center">
             <h1 className="text-2xl md:text-4xl mb-1 md:mb-3
-            text-indigo-600 font-semibold">
+            text-indigo-600 font-semibold dark:text-indigo-500">
                 Hi, this is Hawik</h1>
-            <p className="text-md md:text-xl max-w-md mb-3 text-gray-600">
+            <p className="text-md md:text-xl max-w-md mb-3 text-gray-600 dark:text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Consequuntur perspiciatis totam saepe, placeat quia repellendus quisquam adipisci.
                 Reprehenderit, inventore consequatur.</p>
